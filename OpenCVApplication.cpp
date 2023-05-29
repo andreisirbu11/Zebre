99c85d1513c64fb05bbd8f6c30269033b5991368
Rus Alex <@alexrus2007>
2023-05-29 10:43:26 +0300

    final project + ransac

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -71,39 +71,6 @@ void testColor2Gray()
 /////////////////////////////////////////////////////////////////////////////////
 
 
-int isInside(cv::Point p, int rows, int cols) {
-	if (p.x < rows && p.y < cols)
-		return 1;
-	return 0;
-}
-
-//calculeaza media noantelor de gri
-int computeMean(Mat_<uchar> src)
-{
-	int s = 0;
-	for (int i = 0; i < src.rows; i++)
-		for (int j = 0; j < src.cols; j++)
-			s += src(i, j);
-	return s / (src.rows * src.cols);
-
-}
-
-//converteste din gray-scale in albnegru
-Mat convertoBinary(Mat_<uchar> src) {
-
-	Mat_<uchar> dst(src.cols, src.rows);
-
-	int threshold = computeMean(src);
-
-	for (int i = 0; i < src.rows; i++)
-		for (int j = 0; j < src.cols; j++)
-			if (src(i, j) <= threshold)
-				dst(i, j) = 0;
-			else
-				dst(i, j) = 255;
-	return dst;
-
-}
 
 //pasul1: gasesc prima linie(cea mai de sus)
 //pasul 2: imi aleg latimea dintre linii
@@ -133,7 +100,7 @@ Mat drawIdealModel(int rows, int cols, std::vector<Vec4i> crosswalk_lines)
 	Mat_<uchar> dst = Mat::ones(rows, cols, CV_8UC1) * 255;
 
 	for (auto line1 : crosswalk_lines) {
-		border2 = line1[1]; //un y
+		border2 = line1[1];
 		for (int i = border1; i < border2; i++)
 			for (int j = 0; j < cols; j++)
 				if (curr % 2 == 0)
@@ -153,7 +120,7 @@ Mat drawIdealModel(int rows, int cols, std::vector<Vec4i> crosswalk_lines)
 }
 
 
-//poate are acuratete mai mare cu generatePerspective???
+
 //getPerspectiveTransform are nevoie de 2 vectori de 4 puncte fiecare
 // punctele le obtin din vectorii dreptelor std::vector<Vec4i>(pentru test aleg primele 3 puncte) 
 Mat generatePerspectiveTransformation(std::vector<Vec4i> idealModel, std::vector<Vec4i> zebraLines)
@@ -170,41 +137,15 @@ Mat generatePerspectiveTransformation(std::vector<Vec4i> idealModel, std::vector
 	srcTri[3] = Point2f(idealModel[1][2] * 1.0, idealModel[1][3] * 1.0);
 
 	Point2f dstTri[4];
-	dstTri[0] = Point2f(zebraLines[2][0] * 1.0, zebraLines[2][1] * 1.0);
-	dstTri[1] = Point2f(zebraLines[2][2] * 1.0, zebraLines[2][3] * 1.0);
-	dstTri[2] = Point2f(zebraLines[3][0] * 1.0, zebraLines[3][1] * 1.0);
-	dstTri[3] = Point2f(zebraLines[3][2] * 1.0, zebraLines[3][3] * 1.0);
+	dstTri[0] = Point2f(zebraLines[1][0] * 1.0, zebraLines[1][1] * 1.0);
+	dstTri[1] = Point2f(zebraLines[1][2] * 1.0, zebraLines[1][3] * 1.0);
+	dstTri[2] = Point2f(zebraLines[2][0] * 1.0, zebraLines[2][1] * 1.0);
+	dstTri[3] = Point2f(zebraLines[2][2] * 1.0, zebraLines[2][3] * 1.0);
 
 	return getPerspectiveTransform(srcTri, dstTri);
 }
 
-//calculeaza diferenta de negru-alb a zebrei proiectate
-int countBlackWhites(Mat_<uchar> whiteMask, Mat_<uchar> zebraProjected)
-{
-	int countWhite = 0;
-	int countBlack = 0;
-	for (int i = 0; i < whiteMask.rows; i++)
-		for (int j = 0; j < whiteMask.cols; j++)
-			if (whiteMask(i, j) == 255)
-				if (zebraProjected(i, j) == 0)
-					countBlack++;
-				else
-					countWhite++;
-	return countBlack-countWhite;
-}
 
-//calculeaza aria mastii albe
-int checkWhiteArea(Mat_<uchar> whiteMask,int downLimit,int upLimit)
-{
-	int area = 0;
-	for (int i = 0; i < whiteMask.rows; i++)
-		for (int j = 0; j < whiteMask.cols; j++)
-			if (whiteMask(i, j) == 255)
-				area++;
-	if (downLimit < area && area < upLimit)
-		return 1;
-	return 0;
-}
 
 // RMSE
 int generateRMSE(Mat_<uchar> src1, Mat_<uchar> whiteMask, Mat_<uchar> zebraProjected)
@@ -231,6 +172,7 @@ int generateRMSE(Mat_<uchar> src1, Mat_<uchar> whiteMask, Mat_<uchar> zebraProje
 	return  sum / count;
 
 }
+
 
 
 void printTransformationMatrix(Mat warp_mat)
@@ -295,18 +237,8 @@ std::vector<Vec4i> filterLines(std::vector<Vec4i>  inputLines)
 		}
 	}
 
-	/*
-	  for (auto m : mLines)
-		  printf("%.3g ", m);
-	  printf("\n");
 
-	  for (auto m : result)
-		  printf("%.3g ", m);
-	  printf("\n");
-	  */
-
-
-	  // CONSTRUIREA LINIILOR
+	// CONSTRUIREA LINIILOR
 	for (auto line1 : inputLines) {
 		float m;
 		if (line1[2] - line1[0] != 0) {
@@ -356,44 +288,45 @@ Mat RANSAC1(Mat_<uchar> src, std::vector<Vec4i> filteredLines, std::vector<Vec4i
 	dstTri[3] = Point2f(0, 0);
 
 
-	for (m = 0; m < size/2; m++)
-		for (n = 0; n < size/2; n++)
-			if (m != n && (m+n)%2==1 && abs(m-n)<3) {
+	for (m = 0; m < size / 2; m++)
+		for (n = 0; n < size / 2; n++)
+			if ((m != n) && (m + n) % 2 == 1 && abs(m - n) < 3) {
 
 				//float m1 = 1.0 * (filteredLines[m][3] - filteredLines[m][1]) / (filteredLines[m][2] - filteredLines[m][0]);
 				//float m2 = 1.0 * (filteredLines[n][3] - filteredLines[n][1]) / (filteredLines[n][2] - filteredLines[n][0]);
 
-				
-
-		             //asocierile pentru liniile detectate
-					dstTri[0] = Point2f(filteredLines[m][0] * 1.0, filteredLines[m][1] * 1.0);
-					dstTri[1] = Point2f(filteredLines[m][2] * 1.0, filteredLines[m][3] * 1.0);
-					dstTri[2] = Point2f(filteredLines[n][0] * 1.0, filteredLines[n][1] * 1.0);
-					dstTri[3] = Point2f(filteredLines[n][2] * 1.0, filteredLines[n][3] * 1.0);
-
-					//generez o matrice perspectiva
-					Mat currenMatrix = getPerspectiveTransform(srcTri, dstTri);
-
-					//generez masca alba proiectata
-					warpPerspective(white, white_mask, currenMatrix, warp_dst.size());
-
-					//generez zebra proiectata
-					warpPerspective(idealModelDrawed, warp_dst, currenMatrix, warp_dst.size());
-
-					int currentRMSE = generateRMSE(src, white_mask, warp_dst);
 
 
+					 //asocierile pentru liniile detectate
+				dstTri[0] = Point2f(filteredLines[m][0] * 1.0, filteredLines[m][1] * 1.0);
+				dstTri[1] = Point2f(filteredLines[m][2] * 1.0, filteredLines[m][3] * 1.0);
+				dstTri[2] = Point2f(filteredLines[n][0] * 1.0, filteredLines[n][1] * 1.0);
+				dstTri[3] = Point2f(filteredLines[n][2] * 1.0, filteredLines[n][3] * 1.0);
 
-					printf(" RMSE %d\n", currentRMSE);
-					if (currentRMSE < best_rmse) {
-						best_rmse = currentRMSE;
-						best_matrix = currenMatrix;
+				//generez o matrice perspectiva
+				Mat currenMatrix = getPerspectiveTransform(srcTri, dstTri);
 
-					}
-				
+				//generez masca alba proiectata
+				warpPerspective(white, white_mask, currenMatrix, warp_dst.size());
+
+				//generez zebra proiectata
+				warpPerspective(idealModelDrawed, warp_dst, currenMatrix, warp_dst.size());
+
+				int currentRMSE = generateRMSE(src, white_mask, warp_dst);
+
+
+
+				printf("CURRENT RMSE %d\n", currentRMSE);
+				if (currentRMSE < best_rmse) {
+					best_rmse = currentRMSE;
+					best_matrix = currenMatrix;
+
+				}
+
 
 			}
 
+	printf("FINAL RMSE %d\n", best_rmse);
 	return best_matrix;
 }
 
@@ -430,7 +363,8 @@ void testZebre()
 
 		//filtru gausian pentru eliminarea zgomotelor
 		Mat blur;
-		GaussianBlur(src, blur, Size(5, 5), 0); //in loc deroi_img i am pus src
+		GaussianBlur(src, blur, Size(5, 5), 0);
+
 
 
 		//detectare muchii cu canny
@@ -438,10 +372,10 @@ void testZebre()
 		Canny(blur, edges, 50, 150, 3);
 
 
-
 		//salavare linii drepte cu HOUGHLINESP
 		std::vector<Vec4i> lines2;
-		//HoughLinesP(edges, lines2, 1, CV_PI / 20 / 180, 80, 40, 200);
+
+
 		HoughLinesP(edges, lines2, 1, CV_PI / 180 / 10, 80, 40, 30);
 
 
@@ -462,8 +396,7 @@ void testZebre()
 
 		Mat idealModelDrawed = drawIdealModel(src.rows, src.cols, idealModel);
 
-		Mat warp_matrix = RANSAC1(src, filteredLines, idealModel);  //generatePerspectiveTransformation(idealModel, filteredLines);
-
+		Mat warp_matrix = RANSAC1(src, filteredLines, idealModel);
 
 		//afisez matricea  transformatei afine
 		//printTransformationMatrix(warp_matrix);
@@ -478,21 +411,19 @@ void testZebre()
 		//imagine full alb
 		Mat white = Mat::ones(src.rows, src.cols, src.type()) * 255;
 
+		//aplicarea proiectiei
 		warpPerspective(idealModelDrawed, warp_dst, warp_matrix, warp_dst.size());
 		warpPerspective(white, white_mask, warp_matrix, warp_dst.size());
 
-		//afisez un rmse fara ransac
-		//int rmse = generateRMSE(src, white_mask, warp_dst);
-		//printf("RMSE %d \n", rmse); 
 
 
 
-		//imshow("Canny", edges);
+		imshow("Canny", edges);
 		imshow("WhiteMask", white_mask);
-		imshow("Perspective", warp_dst);
-		imshow("Initial", src);
+		//imshow("Perspective", warp_dst);
+		//imshow("Initial", src);
 		imshow("FINAL", drawFinalCrossWalk(src, white_mask, warp_dst));
-		//imshow("HoughLines", dst2);
+		imshow("HoughLines", dst2);
 		imshow("Filtered", dst3);
 		//waitKey(0);
 	}
@@ -514,6 +445,110 @@ void testIdealModel() {
 
 
 
+//ransac pe linii
+std::vector<Point2d> selectPoints(Mat_<uchar> src) {
+	std::vector<Point2d> result;
+	int height = src.rows;
+	int width = src.cols;
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			if (src(i, j) != 255) {
+				Point2d point;
+				point.x = i;
+				point.y = j;
+				result.push_back(point);
+			}
+		}
+	}
+	return result;
+}
+
+std::vector<Point2d> RANSAC2(Mat_<uchar> src, int k, double threshold, int d) {
+	std::vector<Point2d> bestFit;
+	// src  <-> data input image
+	// model <-> in this case line 
+	// n <-> the minimum number of data points required to estimate the model, in this case 2
+	// k <-> the maximum number of iterations
+	// threshold <-> in this case maximum distance 
+	// d <-> the number of close inliers required to assert that the model fits well to the data
+	// returns the best fit 
+	std::vector<Point2d> data = selectPoints(src);
+	int numberOfPoints = data.size();
+	//printf("SIZE = %d\n", numberOfPoints);
+	int height = src.rows;
+	int width = src.cols;
+	int iterations = 0;
+	Point2i bestFitPoint1, bestFitPoint2;
+	// this parameter is used to sharpen the model parameters to the best data fitting as
+	// iterations goes on
+	int bestErr = -1;
+	// initialize random seed 
+	srand(time(NULL));
+	while (iterations < k) {
+		Point2d maybeInlier1, maybeInlier2;
+		// select randomly 2 points from data
+		int in1 = rand() % numberOfPoints;
+		int in2 = rand() % numberOfPoints;
+		maybeInlier1 = data[in1];
+		maybeInlier2 = data[in2];
+		// maybe model
+		double a, b, c = 0.0f;
+		a = maybeInlier1.y - maybeInlier2.y;
+		b = maybeInlier2.x - maybeInlier1.x;
+		c = maybeInlier1.x * maybeInlier2.y - maybeInlier2.x * maybeInlier1.y;
+		//for each iteration we initialize the vector of inliers to null
+		int nrOfInliers = 0;
+		for (int i = 0; i < numberOfPoints; i++) {
+			Point2d pixel;
+			pixel.x = data[i].x;
+			pixel.y = data[i].y;
+			// we verify if the distance from the point is smaller or equal to the threshold
+			double distance;
+			distance = abs(a * pixel.x + b * pixel.y + c) / sqrt(a * a + b * b);
+			if (distance <= threshold) {
+				nrOfInliers++;
+			}
+		}
+		if (nrOfInliers > d) {
+			// This implies that we may have found a good model.
+			// Now test how good it is
+			Point2i betterModelPoint1, betterModelPoint2;
+			betterModelPoint1.x = maybeInlier1.x;
+			betterModelPoint1.y = maybeInlier1.y;
+			betterModelPoint2.x = maybeInlier2.x;
+			betterModelPoint2.y = maybeInlier2.y;
+			//a measure of how well the better model fits these points
+			int thisErr = nrOfInliers;
+			if (thisErr > bestErr) {
+				bestFitPoint1.x = betterModelPoint1.x;
+				bestFitPoint1.y = betterModelPoint1.y;
+				bestFitPoint2.x = betterModelPoint2.x;
+				bestFitPoint2.y = betterModelPoint2.y;
+				bestErr = thisErr;
+			}
+		}
+		iterations++;
+	}
+	//printf("(%d %d), (%d %d)", bestFitPoint1.x, bestFitPoint1.y, bestFitPoint2.x, bestFitPoint2.y);
+	bestFit.push_back(bestFitPoint1);
+	bestFit.push_back(bestFitPoint2);
+	return bestFit;
+}
+
+void testLiniiRANSAC() {
+	Mat_<uchar> src = Mat(256, 256, CV_8UC1, Scalar(255));
+	int height = src.rows;
+	int width = src.cols;
+	for (int i = 0; i < height; i++) {
+		src[i][i] = 0;
+	}
+	std::vector<Point2d> bestFit = RANSAC2(src, 10, 10.0f, 2);
+	line(src, bestFit[0], bestFit[1], Scalar(128), 1);
+	imshow("src", src);
+	waitKey();
+}
+
+
 int main()
 {
 	int op;
@@ -528,6 +563,7 @@ int main()
 		printf("\n PROIECT \n");
 		printf(" 4 - Zebre\n");
 		printf(" 5 - Ideal Model test\n");
+		printf(" 6 - Ransac pe linii test\n");
 		printf(" 0 - Exit\n\n");
 		printf("Option: ");
 		scanf("%d", &op);
@@ -547,6 +583,9 @@ int main()
 			break;
 		case 5:
 			testIdealModel();
+			break;
+		case 6:
+			testLiniiRANSAC();
 			break;
 
 
